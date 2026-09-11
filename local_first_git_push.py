5bb0587160a7a62c34af5a3c92c783e3414edfff
kid1997 <kid1997@139.com>
2026-09-12 03:00:37 +0800

    old.删除条件判断选择local_first_git_push_V1.0.002_20260912_030037

--- local_first_git_push.py
+++ local_first_git_push.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     print(f"读取到模拟器标识：{CELL_READ_CONST}")
 
     # 固定提交前缀
-    CommitPrefix = "删除条件判断选择local_first_git_push_V1.0.002"
+    CommitPrefix = "old.删除条件判断选择local_first_git_push_V1.0.002"
     print(f"选定提交前缀：{CommitPrefix}")
     # 执行完整git同步
     git_auto_sync(CommitPrefix)
