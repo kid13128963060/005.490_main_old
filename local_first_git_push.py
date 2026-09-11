53cb23d9cb35c4858b019eb89f081e7cc01763ab
kid1997 <kid1997@139.com>
2026-09-12 04:57:08 +0800

    old.测试_增加调试配置文件_20260912_045708

--- local_first_git_push.py
+++ local_first_git_push.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     print(f"读取到模拟器标识：{CELL_READ_CONST}")
 
     # 固定提交前缀
-    CommitPrefix = "old.增加调试配置文件"
+    CommitPrefix = "old.测试_增加调试配置文件"
     print(f"选定提交前缀：{CommitPrefix}")
     # 执行完整git同步
     git_auto_sync(CommitPrefix)
