ab2318e0a3f273f35a5f20b56dfd8b386dc5f39a
kid1997 <kid1997@139.com>
2026-09-12 08:47:33 +0800

    old.测试中变量移顶_20260912_084732

--- local_first_git_push.py
+++ local_first_git_push.py
@@ -1,8 +1,8 @@
-# 说明本地修改上传推送local_first_git_push_V1.0.003
 from read_excel_const import read_simulator_cell
 from git_auto_sync0 import git_auto_sync
 
 EXCEL_FILE = r"E:\自动同步_只增加\设备识别\设备识别.xls"
+CommitPrefix = "old.测试中变量移顶"
 
 if __name__ == "__main__":
     # 读取excel常量
@@ -10,7 +10,6 @@ if __name__ == "__main__":
     print(f"读取到模拟器标识：{CELL_READ_CONST}")
 
     # 固定提交前缀
-    CommitPrefix = "old.测试中回滚脚本"
     print(f"选定提交前缀：{CommitPrefix}")
     # 执行完整git同步
     git_auto_sync(CommitPrefix)
